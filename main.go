ddbc5de77f4c415ed103b6a9ae6a374bce582c35
NanoNite <fcknhandsome@gmail.com>
2026-01-04 16:58:12 +0700

    add Fiber Framework

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	fmt.Println("Hello, World!!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Println("Listening on port", port)
+	app.Listen(":" + port)
 }
